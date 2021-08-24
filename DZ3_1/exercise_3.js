0ae0256f67728840146b5a4c8a2d330540549420
alexeydurakov <alexey.durakov@yandex.ru>
2021-08-24 19:47:08 +0300

    Lesson_3_1

---
+++ DZ3_1/exercise_3.js
@@ -0,0 +1,26 @@
+
+/* 
+ Перед вами находится массив с продуктами, сегодня распродажа и вам нужно на каждый товар применить скидку 
+15%, можете использовать метод forEach 
+*/
+
+"use strict";
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach(function (element) {
+    console.log(element.price - (element.price * 15 / 100));
+});
